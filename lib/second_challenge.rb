274a09ab395838a8ae285a0f51fe08febfcc6b4a
Jake Pino <jakepino@yahoo.com>
2020-02-13 22:46:02 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,16 +2,16 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriach => {}
-      :matriach => {}
-      :hero => {}
+      :patriach => {},
+      :matriach => {},
+      :hero => {},
       :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
-      :patriach => {}
-      :matriach => {}
-      :heroine => {}
+      :patriach => {},
+      :matriach => {},
+      :heroine => {},
       :heroine_friends => []
       }
     
